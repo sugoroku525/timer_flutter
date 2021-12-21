0fe20a29a1f83d6dbb675a88a098f995e2e96634
sugoroku525 <iwamasa816@keio.jp>
2021-12-21 10:09:50 +0900

    periodic error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,50 @@
-import 'dart:html';
-import 'dart:js';
-import 'nextpage.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 import 'dart:async';
 
 void main() {
-  runApp(MyHome());
+  runApp(Stop());
 }
 
-class MyHome extends StatelessWidget {
+class Stop extends StatefulWidget{
+  @override 
+  _StopState createState()=>_StopState();
+}
+class _StopState extends State<Stop> {
+  String _time='';
+  var _timer;
+  @override
 
+  void initState(){
+   _timer = Timer.periodic(Duration(seconds:1),_onTimer,);
+    super.initState();
+  }
+  void dispose(){
+    _timer.cancel();
+    super.dispose();
+  }
+  void _onTimer(Timer timer){
+    var now=DateTime.now();
+    var formatter = DateFormat('HH:mm:ss');
+    var formattedTime = formatter.format(now);
+    setState(()=>_time = formattedTime);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title:'やめる',
-      theme:ThemeData(
-        fontFamily: 'Honoka-Shin-Maru-Gothic_R'
+    return Center(
+      child: Text(
+        _time,
+        style: TextStyle(
+          fontSize: 60.0,
+          
+        )
       ),
-      home:Stopscreen(),
-      initialRoute: Stopscreen.id,
-      routes:{
-          NextPage.id:(context)=> NextPage(),
-      }
     );
   }
 }
 
 
 
-class Stopscreen extends StatelessWidget {
- 
-  static const String id = 'stop_screen';
-  bool time = false;
-  void onTimer(){
-    time = true;
-  }
 
   
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('やめる'),
-        backgroundColor: Colors.blue,
-      ),
-      body:Center(
-        
-        child: Column(
-          mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:<Widget> [
-            Text('タイマーを押して深呼吸しよう',style: TextStyle(fontSize: 30.0,color: Colors.blue,fontFamily:'Honoka-Shin-Maru-Gothic_R'),),
-             
-
-            Row(
-      
-              mainAxisAlignment:MainAxisAlignment.center,
-              children:<Widget> [
-                
-                Padding(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: ElevatedButton(
-                   style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      onPrimary: Colors.black
-                    ),
-                    onPressed: (){
-                      Timer(Duration(minutes:1),onTimer
-                        
-                      );
-                      Navigator.pushNamed(context, NextPage.id);
-                    },
-                    child: Icon(Icons.play_arrow_outlined,size: 60.0,color: Colors.blue,
-                    ),
-                  ),
-                ),
-            
-                Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      onPrimary: Colors.black
-                    ),
-                    onPressed: (){},
-                    child: Icon(Icons.refresh,size: 60.0,color: Colors.blue,)),
-                )
-              ],
-            )
-          ]
-
-        ),)
-    );
-  }
-}
-

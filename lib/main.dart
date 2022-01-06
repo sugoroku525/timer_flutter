bc24487a0fbda3b824c14d2c2e806f623e97311a
sugoroku525 <iwamasa816@keio.jp>
2022-01-06 23:11:00 +0900

    countdown timer make

--- lib/main.dart
+++ lib/main.dart
@@ -15,50 +15,92 @@ class Stop extends StatefulWidget{
 class _StopState extends State<Stop> {
   var _timer;
   var _time;
+  var _currentSeconds;
   @override
 
-  void initState(){
-   _time = DateTime.utc(0,0,0);
-    super.initState();
+  void dispose(){
+    super.dispose();
+    _timer.cancer();
   }
 
 
   @override
+  void initState(){
+    super.initState();
+    final workMinuts = 0;
+    _currentSeconds = workMinuts * 60+30;
+    _timer = countTimer();
+  }
+  Timer countTimer(){
+    return Timer.periodic(
+      const Duration(seconds:1),
+      (Timer timer){
+        if(_currentSeconds<1){
+          timer.cancel();
+        }
+        else{
+          setState((){
+            _currentSeconds = _currentSeconds -1;
+          });
+        }
+      }
+    );
+  }
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child:Column(
+        mainAxisAlignment: MainAxisAlignment.start,
 
         children: <Widget>[
-          Text(
-            DateFormat.Hms().format(_time),
-              style: Theme.of(context).textTheme.headline2,
-              ),
-          Row(
-            mainAxisAlignment:MainAxisAlignment.center,
-            children: <Widget>[
-              FloatingActionButton(onPressed:(){
-                if(_timer !=null && _timer.isActive)_timer.cancel();
-              } ,
-              child:Text("Stop"),),
-              FloatingActionButton(onPressed: (){
-                _timer  = Timer.periodic(Duration(seconds:1),
-                (Timer timer){
-                  setState((){
-                    _time = _time.add(Duration(seconds:1));
-                  });
-                }
-                );
-              }
-              ,child:Text("Start"),
-              )
-          ],)
+          const SizedBox(height:16),
+          _timeStr(),
+          const SizedBox(height:16),
+          _panel(),
+          
 
         ]
       ),
+      )
+      
     );
   }
+  Widget _timeStr(){
+    return Text(
+      timeString(_currentSeconds),
+      style:TextStyle(fontSize: 32,color: Colors.black),
+    );
+  }
+  String timeString(int leftSeconds){
+    final minutes = (leftSeconds /60).floor().toString().padLeft(2,'0');
+    final seconds = (leftSeconds %60).floor().toString().padLeft(2,'0');
+    return '$minutes:$seconds';
+  }
+  Widget _panel(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(onPressed: (){
+            if(_timer.isActive){
+              _timer.cancel();
+            }
+          }, icon: Icon(Icons.stop)) ,
+          IconButton(onPressed:(){
+            if(!_timer.isActive){
+              setState(() {
+                _timer = countTimer();
+              });}},
+           icon: Icon(Icons.play_arrow)) ,
+           IconButton(onPressed: (){
+             setState((){
+               _currentSeconds = 30;
+             });
+           }, icon:Icon(Icons.restart_alt))
+  
+      ],);
+  }
 }
+
 
 
 
